c7ae76114b9e4469ee76f64e4c7b0a666626c6ef
Elliotplaysvideogames <87897295+Elliotplaysvideogames@users.noreply.github.com>
2021-07-27 11:50:02 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var ship
+var sea,seaImg,shipImg1
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea = createSprite(200,200,400,400)
+  sea.addImage(seaImg)
+  sea.scale = 0.35
+  sea.velocityX = -2
+  ship = createSprite(200,200)
+  ship.addAnimation("ship",shipImg1)
+  ship.scale = 0.2
+    
+}
+
+function draw() {
+  background("blue");
+
+if(sea.x < 0){
+  sea.x = sea.width/7;
+}
+
+drawSprites()
+}
